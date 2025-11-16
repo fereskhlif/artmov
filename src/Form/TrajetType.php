99ee77bd09aebcc3b76e4bf99b3ba64d0f4389df
Feres Khlif <fereskhlif@gmail.com>
2025-11-16 14:45:22 +0100

    integration d'une template

--- src/Form/TrajetType.php
+++ src/Form/TrajetType.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Form;
+
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -9,7 +10,6 @@ use App\Entity\Trajet;
 use App\Entity\Vehicule;
 
 use Symfony\Component\Form\AbstractType;
-
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,15 +18,44 @@ class TrajetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_dep', DateTimeType::class, ['widget' => 'single_text'])
-            ->add('date_arr', DateTimeType::class, ['widget' => 'single_text'])
-            ->add('ville_dep')
-            ->add('ville_arr')
+            ->add('date_dep', DateTimeType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control form-control-sm', // version compacte Bootstrap
+                    'style' => 'width: 200px;' // largeur personnalisée
+                ]
+            ])
+            ->add('date_arr', DateTimeType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control form-control-sm',
+                    'style' => 'width: 200px;'
+                ]
+            ])
+            ->add('ville_dep', null, [
+                'attr' => [
+                    'class' => 'form-control form-control-sm',
+                    'style' => 'width: 150px;'
+                ]
+            ])
+            ->add('ville_arr', null, [
+                'attr' => [
+                    'class' => 'form-control form-control-sm',
+                    'style' => 'width: 150px;'
+                ]
+            ])
             ->add('vehicule', EntityType::class, [
                 'class' => Vehicule::class,
                 'choice_label' => 'matricule',
+                'attr' => [
+                    'class' => 'form-control form-control-sm',
+                    'style' => 'width: 150px;'
+                ]
             ])
-            ->add('submit', SubmitType::class, ['label' => 'Ajouter Trajet']);
+            ->add('submit', SubmitType::class, [
+                'label' => 'Ajouter Trajet',
+                'attr' => ['class' => 'btn btn-primary btn-sm mt-3']
+            ]);
 
     }
 
